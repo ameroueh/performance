9dfd465480694e38e04d37b6a75ee5abf67767a4
ameroueh <ares.meroueh@gmail.com>
2019-04-08 08:32:45 +0200

    Delete boost_envs.sh

--- install-boost/boost_envs.sh
+++
@@ -1,5 +0,0 @@
-export BOOST_ROOT=/tmp/boost_1_69_0
-export BOOST_LIB=/tmp/boost_1_69_0/stage/lib
-export PYTHON_INCLUDE=/opt/anaconda/envs/Python3/include/python3.6m
-export NUMPY_INCLUDE=/opt/anaconda/envs/Python3/lib/python3.6/site-packages/numpy/core/include
-export LD_LIBRARY_PATH=$BOOST_LIB:$LD_LIBRARY_PATH
